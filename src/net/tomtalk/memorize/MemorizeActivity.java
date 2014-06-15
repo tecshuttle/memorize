4d145eb3df6970d18590123ae32802687a7abf1d
tecshuttle <tecshuttle@gmail.com>
2014-06-15 11:33:02 +0800

    remove code: scroll question layout when play.

--- src/net/tomtalk/memorize/MemorizeActivity.java
+++ src/net/tomtalk/memorize/MemorizeActivity.java
@@ -331,32 +331,6 @@ public class MemorizeActivity extends Activity implements OnGestureListener {
     // 绑定触屏事件
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-	LinearLayout ly = (LinearLayout) findViewById(R.id.ly_question);
-
-	float x = event.getX();
-	// float y = event.getY();
-
-	switch (event.getAction()) {
-	case MotionEvent.ACTION_DOWN:
-	    // 如果屏幕的动画还没结束，你就按下了，我们就结束上一次动画，即开始这次新ACTION_DOWN的动画
-	    /*
-	     * if (mScroller != null) { if (!mScroller.isFinished()) {
-	     * mScroller.abortAnimation(); } }
-	     */
-	    mLastionMotionX = x; // 记住开始落下的屏幕点
-	    break;
-	case MotionEvent.ACTION_MOVE:
-	    int detaX = (int) (mLastionMotionX - x); // 每次滑动屏幕，屏幕应该移动的距离
-	    ly.scrollBy(detaX, 0);// 开始缓慢滑屏咯。 detaX > 0 向右滑动 ， detaX < 0 向左滑动 ，
-
-	    mLastionMotionX = x;
-	    break;
-	case MotionEvent.ACTION_UP:
-	    mLastionMotionX = 0;
-	    ly.scrollTo(0, 0);
-	    break;
-	}
-
 	return this.gesture_detector.onTouchEvent(event);
     }
 
